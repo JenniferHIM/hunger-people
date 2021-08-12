36940a9059898867b19ee970193c293b3eae5f2c
Evgeniya Obernikhina <jenniferhim1201@gmail.com>
2021-08-12 14:33:28 +0300

    add hero section

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,33 +1,35 @@
-import {NavLink} from 'react-router-dom';
-import styles from './Navigation.module.css';
+import { NavLink } from 'react-router-dom';
+// import styles from './styles/main.scss';
+import styles from './Navigation.module.scss';
 
 const Navigation = () => {
 
     return (
-        <div className={styles.container}>
+        <div className={ styles.container }>
             <nav className={styles.navigation}>
-                <NavLink exact to="/" className={styles.link} activeClassName={styles.activeLink}>
+                
+                <NavLink exact to="/" className={ styles.link } activeClassName={ styles.activeLink }>
                     Home
                 </NavLink>
-                <NavLink to="/about" className={styles.link} activeClassName={styles.activeLink}>
+                <NavLink to="/about" className={ styles.link } activeClassName={ styles.activeLink }>
                     About
                 </NavLink>
-                <NavLink to="/team" className={styles.link} activeClassName={styles.activeLink}>
+                <NavLink to="/team" className={ styles.link } activeClassName={ styles.activeLink }>
                     Team
                 </NavLink>
-                <NavLink to="/booking" className={styles.link} activeClassName={styles.activeLink}>
+                <NavLink to="/booking" className={ styles.link } activeClassName={ styles.activeLink }>
                     Booking
                 </NavLink>
-                <NavLink to="/menu" className={styles.link} activeClassName={styles.activeLink}>
+                <NavLink to="/menu" className={ styles.link } activeClassName={ styles.activeLink }>
                     Menu
                 </NavLink>
-                <NavLink to="/galerie" className={styles.link} activeClassName={styles.activeLink}>
+                <NavLink to="/galerie" className={ styles.link } activeClassName={ styles.activeLink }>
                     Galerie
                 </NavLink>
-                <NavLink to="/event" className={styles.link} activeClassName={styles.activeLink}>
+                <NavLink to="/event" className={ styles.link } activeClassName={ styles.activeLink }>
                     Event
                 </NavLink>
-                <NavLink to="/contact" className={styles.link} activeClassName={styles.activeLink}>
+                <NavLink to="/contact" className={ styles.link } activeClassName={ styles.activeLink }>
                     Contact
                 </NavLink>
             </nav>

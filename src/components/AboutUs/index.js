8af09f0d09109668f03736ffd4b28eacb16ad68e
Evgeniya Obernikhina <jenniferhim1201@gmail.com>
2021-08-16 13:43:17 +0300

    add section specialties

--- src/components/AboutUs/index.js
+++ src/components/AboutUs/index.js
@@ -1,1 +1,35 @@
-export { default } from './AboutUs';
+import React from 'react';
+import styles from './AboutUs.module.scss';
+import about from '../../images/about-img.png';
+import square from '../../images/square.png';
+
+const AboutUs = () => {
+
+    return (
+    <section className={styles.aboutUs}>
+            <div className={styles.container}>
+                <div className={styles.aboutUs__textWrap}>
+                    <h2 className={styles.aboutUs__title}>About Us</h2>
+                    <hr className={ styles.aboutUs__line }/>
+                    <p className={styles.aboutUs__text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at velit maximus, molestie est a, tempor magna. <br/>
+                        <span className={styles.aboutUs__partText}>
+                        Integer ullamcorper neque eu purus euismod, ac faucibus mauris posuere. Morbi non ultrices ligula.
+                        Sed dictum, enim sed ullamcorper feugiat, dui odio vehicula eros, a sollicitudin lorem quam nec sem.
+                        Mauris tincidunt feugiat diam convallis pharetra. Nulla facilisis semper laoreet.
+                        </span>
+                    </p>
+                </div>
+        
+                <div className={styles.aboutUs__imageWrap}>
+                    <img src={about} alt="bonfire" className={styles.aboutUs__image} />
+                    <img src={square} alt="square" className={ styles.aboutUs__square}/> 
+                </div>
+               
+    </div>
+    </section>
+            
+)
+    
+}
+
+export default AboutUs;
--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,1 +1,35 @@
-export { default } from './Hero';
+import React from 'react';
+import { Facebook, Twitter, Instagram } from '@material-ui/icons';
+import styles from './Hero.module.scss';
+import logo from '../../images/logo.png';
+// import { ReactComponent as TableIcon } from '../../images/sprite.svg';
+
+const Hero = () => {
+
+    return (
+        <div className={styles.container}>
+            <img src={logo} alt="logo" className={ styles.logo}/>
+            <p className={styles.vertical}>Mon - Fri: 8PM - 10PM, Sat - Sun: 8PM - 3AM</p>
+            <div className={ styles.heroContainer }>
+                <h1 className={styles.title}><span className={styles.titleMain}>Restaurant</span>Hungry People</h1>
+                <hr className={ styles.line }/>
+                <div className={styles.buttonWrapper}>
+                    <button type="submit" className={ styles.addBtn }>Book table</button>
+                    <button type="submit" className={ styles.addBtn }>Explore</button>
+                </div>
+                <button type="button" className={styles.roundBtn}><img className={styles.downIcon} href="./images/sprite.svg" alt=""/></button>
+            </div>
+            
+           
+
+            <div className={styles.socialBox}>
+                <Facebook className={ styles.socialList } />
+                <Twitter className={ styles.socialList } />
+                <Instagram className={ styles.socialList } />
+            </div>
+
+        </div>
+    )
+}
+
+export default Hero;
